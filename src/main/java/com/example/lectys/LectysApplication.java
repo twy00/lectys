4eeee23a624b453ea30b723902156729ef435b6e
Taewon <twyoo@khu.ac.kr>
2021-07-17 14:01:49 +0900

    first commit

---
+++ src/main/java/com/example/lectys/LectysApplication.java
@@ -0,0 +1,13 @@
+package com.example.lectys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectysApplication.class, args);
+	}
+
+}
